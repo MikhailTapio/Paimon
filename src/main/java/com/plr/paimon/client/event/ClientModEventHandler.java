12c9e2a95521a9685cba281d9989cdae3a58e67e
MikhailTapio <mikhailtapio13468@163.com>
2024-03-17 10:14:31 +0800

    Fix slot

--- src/main/java/com/plr/paimon/client/event/ClientModEventHandler.java
+++ src/main/java/com/plr/paimon/client/event/ClientModEventHandler.java
@@ -3,8 +3,10 @@ package com.plr.paimon.client.event;
 import com.plr.paimon.client.model.ModelPaimon;
 import com.plr.paimon.client.renderer.RenderPaimon;
 import com.plr.paimon.common.entities.ModEntities;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -18,5 +20,10 @@ public class ClientModEventHandler {
     @SubscribeEvent
     public static void registerLayerDefs(EntityRenderersEvent.RegisterLayerDefinitions evt) {
         evt.registerLayerDefinition(ModelPaimon.MODEL_LAYER_LOCATION, ModelPaimon::createBodyLayer);
+    }
+
+    @SubscribeEvent
+    public static void onStitchTexture(TextureStitchEvent.Pre event) {
+        event.addSprite(new ResourceLocation("paimon", "slot/empty_paimon_slot"));
     }
 }
